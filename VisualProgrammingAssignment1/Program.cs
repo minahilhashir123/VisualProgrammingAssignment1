3b602c01296bebba90fec3209c1d587f7597a537
minahilhashir123 <57205173+minahilhashir123@users.noreply.github.com>
2019-10-31 21:00:41 +0500

    Add project files.

---
+++ VisualProgrammingAssignment1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualProgrammingAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            int mainMenuOption = 0, semester = 0;
+            float cgpa;
+            decimal dCGPA = 0;
+            string name = " ", department = " ", university = " ", studentId = " ", attendance = " ";
+            Console.Write("\tMain Menu\n\nSelect any of the following:\n\t1. Create Student profile\n\t2. Search Student\n\t3. Delete Student Record\n\t4. List top 3 Students\n\t5. Mark Student Attendance\n\t6. View Attendance\n\n\tOption: ");
+            mainMenuOption = Convert.ToInt32(Console.ReadLine());
+            s.ReadFile();
+            Console.Clear();
+            switch (mainMenuOption)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("\tCreate Student Profile\n");
+                        Console.Write("Enter the fololwing data for a new student:\n\tStudent ID: ");
+                        studentId = Console.ReadLine();
+                        Console.Write("\tStudent Name: ");
+                        name = Console.ReadLine();
+                        Console.Write("\tAttendance (P/A): ");
+                        attendance = Console.ReadLine();
+                        Console.Write("\tCGPA: ");
+                        dCGPA = Convert.ToDecimal(Console.ReadLine());
+                        cgpa = (float)dCGPA;
+                        Console.Write("\tSemester: ");
+                        semester = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("\tDepartment: ");
+                        department = Console.ReadLine();
+                        Console.Write("\tUniversity: ");
+                        university = Console.ReadLine(); 
+                        s.CreateNewStudent(studentId, name, semester, cgpa, department, university, attendance);
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("\tSearch Student\n");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("\tDelete Student\n");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("\tList top 3 Students\n");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("\tMark Student Attendance\n");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("\tView Attendance\n");
+                        break;
+                    }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VisualProgrammingAssignment1/Student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualProgrammingAssignment1
+{
+    class Student
+    {
+        List<Student> listie;
+        int semester;
+        decimal dCGPA;
+        float cgpa;
+        string name, department, university, studentId, attendance;
+        public Student()
+        {
+            listie = new List<Student>();
+            semester = 0;
+            cgpa = 0;
+            dCGPA = 0;
+            attendance = studentId = name = department = university = " ";
+        }
+        
+        public void ReadFile()
+        {
+            string line = " ";
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\HO\Desktop\StudentInfo.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                studentId = line;
+                name = file.ReadLine();
+                attendance = file.ReadLine();
+                dCGPA = Convert.ToDecimal(file.ReadLine());
+                cgpa = (float)dCGPA;
+                semester = Convert.ToInt32(file.ReadLine());
+                department = file.ReadLine();
+                university = file.ReadLine();
+                listie.Add(new Student() { studentId = this.studentId, name = this.name, semester = this.semester, cgpa = this.cgpa, department = this.department, university = this.university, attendance = this.attendance });
+            }
+            file.Close();
+            
+        }
+        
+        public void CreateNewStudent(string sId, string n, int s, float c, string d, string u, string a)
+        {
+            listie.Add(new Student(){ studentId = sId, name = n, semester = s, cgpa = c, department = d, university = u, attendance = a});
+
+            using (StreamWriter write = new StreamWriter(@"C:\Users\HO\Desktop\StudentInfo.txt",true))
+            {
+                write.WriteLine("\n{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}", sId, n, a, c, s, d, u);
+            }
+            Console.WriteLine("1st Name: {0}\n2nd Name: {1}\n3rd Name: {2}", listie[0].name,listie[1].name,listie[2].name);
+            //Console.WriteLine("1st Name: {0}", listie[0].name);
+        }
+    }
+}
